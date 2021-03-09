5618b010d9a5cd0b0cde0eacbee4a17bb4b34e32
StableNarwhal <J.Boettcher@campus.lmu.de>
2021-03-09 15:57:53 +0000

    Update media_sku_wa_g9.cpp

--- media_driver/linux/gen9/ddi/media_sku_wa_g9.cpp
+++ media_driver/linux/gen9/ddi/media_sku_wa_g9.cpp
@@ -281,6 +281,7 @@ static bool InitBxtMediaSku(struct GfxDeviceInfo *devInfo,
     MEDIA_WR_SKU(skuTable, FtrPerCtxtPreemptionGranularityControl, 1);
 
     MEDIA_WR_SKU(skuTable, FtrVpP010Output, 1);
+    MEDIA_WR_SKU(skuTable, FtrHDR, 1); 
 
     return true;
 }
